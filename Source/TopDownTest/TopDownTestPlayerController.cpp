f1d652aca9d1872f1b44e4678137a890c62fe6c2
James <jwdeharo@gmail.com>
2020-08-18 23:11:39 +0200

    Setted the rotation with the mouse.
    - Added a bool var that indicates that the player can rotate to the mouse position.
    - Added beginplay override.

--- Source/TopDownTest/TopDownTestPlayerController.cpp
+++ Source/TopDownTest/TopDownTestPlayerController.cpp
@@ -1,6 +1,9 @@
 #include "TopDownTestPlayerController.h"
 #include "TopDownTestCharacter.h"
 #include "Engine/World.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "Runtime/Engine/Classes/Components/DecalComponent.h"
 
 ATopDownTestPlayerController::ATopDownTestPlayerController()
 {
@@ -8,9 +11,20 @@ ATopDownTestPlayerController::ATopDownTestPlayerController()
 	DefaultMouseCursor = EMouseCursor::Crosshairs;
 }
 
+void ATopDownTestPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	bCanRotateToLocation = false;
+}
+
 void ATopDownTestPlayerController::PlayerTick(float DeltaTime)
 {
 	Super::PlayerTick(DeltaTime);
+
+	if (bCanRotateToLocation)
+	{
+		ChangePlayerRotation();
+	}
 }
 
 void ATopDownTestPlayerController::SetupInputComponent()
@@ -19,6 +33,9 @@ void ATopDownTestPlayerController::SetupInputComponent()
 
 	InputComponent->BindAxis("MoveForward", this, &ATopDownTestPlayerController::MoveForward);
 	InputComponent->BindAxis("MoveRight", this, &ATopDownTestPlayerController::MoveRight);
+
+	InputComponent->BindAction("SetDestination", IE_Pressed, this, &ATopDownTestPlayerController::ChangeCanRotateLocation);
+	InputComponent->BindAction("SetDestination", IE_Released, this, &ATopDownTestPlayerController::ChangeCanRotateLocation);
 }
 
 void ATopDownTestPlayerController::MoveForward(float AxisValue)
@@ -39,4 +56,43 @@ void ATopDownTestPlayerController::Move(float AxisValue, EAxis::Type AxisType)
 
 	FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(AxisType);
 	MyPawn->AddMovementInput(Direction, AxisValue);
+}
+
+void ATopDownTestPlayerController::ChangeCanRotateLocation()
+{
+	bCanRotateToLocation = !bCanRotateToLocation;
+}
+
+void ATopDownTestPlayerController::ChangePlayerRotation()
+{
+	if (UHeadMountedDisplayFunctionLibrary::IsHeadMountedDisplayEnabled())
+	{
+		ATopDownTestCharacter* MyPawn = Cast<ATopDownTestCharacter>(GetPawn());
+
+		if (MyPawn != nullptr)
+		{
+			if (MyPawn->GetCursorToWorld())
+			{
+				RotateToLocation(MyPawn->GetCursorToWorld()->GetComponentLocation());
+			}
+		}
+	}
+	else
+	{
+		// Trace to see what is under the mouse cursor
+		FHitResult Hit;
+		GetHitResultUnderCursor(ECC_Visibility, false, Hit);
+
+		if (Hit.bBlockingHit)
+		{
+			RotateToLocation(Hit.ImpactPoint);
+		}
+	}
+}
+
+void ATopDownTestPlayerController::RotateToLocation(const FVector& Location)
+{
+	const FRotator& FindLookAt = UKismetMathLibrary::FindLookAtRotation(GetPawn()->GetActorLocation(), Location);
+	FRotator LookAtRotation = FRotator(0.f, FindLookAt.Yaw, 0.f);
+	GetPawn()->SetActorRotation(LookAtRotation);
 }
--- Source/TopDownTest/TopDownTestPlayerController.h
+++ Source/TopDownTest/TopDownTestPlayerController.h
@@ -20,6 +20,11 @@ public:
 
 protected:
 	/**
+	* Function called at the begining of the game.
+	*/
+	void BeginPlay() override;
+
+	/**
 	* Updates the player every frame.
 	* @param DeltaTime: time of the game.
 	*/
@@ -28,7 +33,7 @@ protected:
 	/**
 	* Setups the input that the controller will handle.
 	*/
-	virtual void SetupInputComponent() override;
+	void SetupInputComponent() override;
 
 private:
 
@@ -50,6 +55,24 @@ private:
 	* @param AxisType: Enum value that indicates the axis.
 	*/
 	void Move(float AxisValue, EAxis::Type AxisType);
+
+	/**
+	* Changes if the player can rotate to a location.
+	*/
+	void ChangeCanRotateLocation();
+
+	/**
+	* Rotates the player to the touch position.
+	*/
+	void ChangePlayerRotation();
+
+	/**
+	* Given a location, the function rotates the player to look at that location.
+	* @param: Location: touch location.
+	*/
+	void RotateToLocation(const FVector& Location);
+
+	bool bCanRotateToLocation; //!< Indicates if the player can rotate to location.
 };
 
 
